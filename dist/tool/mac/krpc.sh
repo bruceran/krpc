9e3a54f65ccf494ae3c5545620b4f89962a68272
bruceran <bruce_ran@sina.com>
2018-08-16 08:27:00 +0800

    删除无用文件

--- dist/tool/mac/krpc.sh
+++ dist/tool/mac/krpc.sh
@@ -10,7 +10,7 @@ touch  $1_sources.jar -r $1.proto
 javapackagepath=`grep java_package $1.proto|awk -F\" '{print $2}'|sed 's/\./\//g'`
 
 mkdir target/classes  
-javac -cp "protobuf-java-3.5.1.jar:krpc-0.1.0.jar:." -d target/classes/ target/src/$javapackagepath/*.java
+javac -cp "protobuf-java-3.5.1.jar:krpc-0.1.0.jar:*:." -d target/classes/ target/src/$javapackagepath/*.java
 jar cf $1_protos.jar -C target/classes/ .
 touch  $1_protos.jar -r $1.proto
 
